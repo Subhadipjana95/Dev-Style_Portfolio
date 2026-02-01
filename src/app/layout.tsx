35ec01f3f596781df3faa9bbad0ee3b1c5654c4d
Subhadip Jana <149944137+Subhadipjana95@users.noreply.github.com>
2026-02-02 03:44:59 +0530

    Google analytics g-tags script added.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -134,6 +134,21 @@ export default function RootLayout({
             }),
           }}
         />
+
+        {/* Google tag (gtag.js) */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-W9GKL56QWY"
+        />
+        <Script id="gtag-init">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-W9GKL56QWY');
+          `}
+        </Script>
       </head>
 
       <body
