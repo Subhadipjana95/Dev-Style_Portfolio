fed898ebcf6cacfa3224a1aef66b1417694af486
Subhadip Jana <149944137+Subhadipjana95@users.noreply.github.com>
2026-03-14 18:27:38 +0530

    Update border color of Spotify card at page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -299,7 +299,7 @@ export default function Page() {
           </BlurFade>
           <div className="flex min-h-0 flex-col gap-y-8">
             <BlurFade delay={BLUR_FADE_DELAY * 16}>
-              <Card className=" bg-[#921C11] border border-dashed border-[#921C11] dark:border-[#e13526] shadow-sm max-w-2xl mx-auto rounded-xl overflow-hidden">
+              <Card className=" bg-[#921C11] border border-dashed border-foreground shadow-sm max-w-2xl mx-auto rounded-xl overflow-hidden">
                 <iframe
                   data-testid="embed-iframe"
                   className="w-full block"
